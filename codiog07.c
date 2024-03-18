e332e75a9ecc96bedce030d4888d2aa7f100836e
lmnunes2002 <161032233+lmnunes2002@users.noreply.github.com>
2024-03-18 11:11:52 -0300

    Update codiog07.c

--- codiog07.c
+++ codiog07.c
@@ -2,18 +2,20 @@
 #include <stdlib.h>
 
 int main(){
-	//DeclarÁ„o de vari·vel.
-	int valorQualquer;
+	//Declar√ß√£o de vari√°vel.
+	int valorQualquer, desconto;
 	
-	//DefiniÁ„o de vari·vel.
+	//Defini√ß√£o de vari√°vel.
 	printf("Digite aqui um valor qualquer: ");
 	scanf("%i", &valorQualquer);
 	
 	//Calculadora.
 	valorQualquer *= 0.9;
+	desconto = valorQualquer * 0.1
 	
 	//Display.
 	printf("Valor com reajuste: %i", valorQualquer);
+	printf("Valor do desconto: %i", desconto);
 	
 	return 0;
 }
