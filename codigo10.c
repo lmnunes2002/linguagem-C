86baf6577ff877ad502b1caabc653f521def202e
lmnunes2002 <161032233+lmnunes2002@users.noreply.github.com>
2024-03-19 10:44:05 -0300

    Update codigo10.c

--- codigo10.c
+++ codigo10.c
@@ -6,14 +6,14 @@ int main(){
 	//Linguagem.
 	setlocale(LC_ALL, "portuguese");
 	
-	//DeclaraÁ„o de vari·veis.
+	//Declara√ß√£o de vari√°veis.
 	int numeroUm, numeroDois, media, soma, produto, divisao;
 
-	//DefiniÁ„o de vari·veis.
-	printf("Digite aqui a primeira vari·vel: ");
+	//Defini√ß√£o de vari√°veis.
+	printf("Digite aqui a primeira vari√°vel: ");
 	scanf("%i", &numeroUm);
 
-	printf("Digite aqui a segunda vari·vel: ");
+	printf("Digite aqui a segunda vari√°vel: ");
 	scanf("%i", &numeroDois);
 
 	system("cls");
@@ -21,21 +21,21 @@ int main(){
 	//Calculadora.
 	soma = numeroUm + numeroDois;
 	produto = numeroUm * numeroDois;
-	media = (numeroUm + numeroDois) / 2;
+	media = (numeroUm + numeroDois) / float 2;
 
-	//Exibindo vari·veis.
-	printf("Vari·vel um: %i", numeroUm);
-	printf("\nVari·vel dois: %i", numeroDois);
-	printf("\nSoma das vari·veis: %i", soma);
-	printf("\nProduto das vari·veis: %i", produto);
+	//Exibindo vari√°veis.
+	printf("Vari√°vel um: %i", numeroUm);
+	printf("\nVari√°vel dois: %i", numeroDois);
+	printf("\nSoma das vari√°veis: %i", soma);
+	printf("\nProduto das vari√°veis: %i", produto);
 
 	//Condicional.
 	if (numeroUm == numeroDois){
-		printf("\nAs vari·veis s„o iguais");
+		printf("\nAs vari√°veis s√£o iguais");
 	} else if (numeroUm > numeroDois){
-		printf("\nA vari·vel um È maior que a vari·vel dois");
+		printf("\nA vari√°vel um √© maior que a vari√°vel dois");
 	} else {
-		printf("\nA vari·vel dois È maior que a vari·vel um");
+		printf("\nA vari√°vel dois √© maior que a vari√°vel um");
 	}
 
 	return 0;
