57ce014f8df193be4cba45378121f30a40df4574
lmnunes2002 <161032233+lmnunes2002@users.noreply.github.com>
2024-03-19 10:24:25 -0300

    Add files via upload

---
+++ codigo10.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	//Linguagem.
+	setlocale(LC_ALL, "portuguese");
+	
+	//Declaração de variáveis.
+	int numeroUm, numeroDois, media, soma, produto, divisao;
+
+	//Definição de variáveis.
+	printf("Digite aqui a primeira variável: ");
+	scanf("%i", &numeroUm);
+
+	printf("Digite aqui a segunda variável: ");
+	scanf("%i", &numeroDois);
+
+	system("cls");
+
+	//Calculadora.
+	soma = numeroUm + numeroDois;
+	produto = numeroUm * numeroDois;
+	media = (numeroUm + numeroDois) / 2;
+
+	//Exibindo variáveis.
+	printf("Variável um: %i", numeroUm);
+	printf("\nVariável dois: %i", numeroDois);
+	printf("\nSoma das variáveis: %i", soma);
+	printf("\nProduto das variáveis: %i", produto);
+
+	//Condicional.
+	if (numeroUm == numeroDois){
+		printf("\nAs variáveis são iguais");
+	} else if (numeroUm > numeroDois){
+		printf("\nA variável um é maior que a variável dois");
+	} else {
+		printf("\nA variável dois é maior que a variável um");
+	}
+
+	return 0;
+}
