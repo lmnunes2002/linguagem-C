b46564c431f579e326b1fcc657a974374f0ace81
lmnunes2002 <161032233+lmnunes2002@users.noreply.github.com>
2024-03-22 11:20:59 -0300

    Add files via upload

--- codigo16.c
+++ codigo16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-#include <string.h>
+#include <ctype.h>
 
 int main(){
 	//Linguagem.
@@ -16,6 +16,9 @@ int main(){
 	scanf("%c", &sexo);
 	printf("Digite aqui sua altura: ");
 	scanf("%f", &altura);
+	
+	//Converte em maiúsculo.
+	sexo = toupper(sexo);
 	
 	system("cls");
 	
