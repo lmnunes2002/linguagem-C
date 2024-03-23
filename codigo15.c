c242daa9b08633153c0156020200f083c51bc959
lmnunes2002 <161032233+lmnunes2002@users.noreply.github.com>
2024-03-23 10:50:45 -0300

    Update codigo15.c

--- codigo15.c
+++ codigo15.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
 
 int main(){
+	//Linguagem.
+	setlocale(LC_ALL, "portugues");
+	
 	//Definição de variáveis.
 	char nomeAluno[50], aprovacao[50];
 	char conceito;
@@ -11,13 +15,12 @@ int main(){
 	//Declaraçaõ de variáveis.
 	printf("Digite aqui o nome do aluno: ");
 	fgets(nomeAluno, 50, stdin);
-
-	fflush(stdin);
-	
 	printf("Digite aqui a primeira nota: ");
 	scanf("%f", &notaUm);
 	printf("Digite aqui a segunda nota: ");
 	scanf("%f", &notaDois);
+	
+	fflush(stdin);
 	
 	//Calculadora.
 	media = (notaUm + notaDois) / 2;
@@ -26,13 +29,13 @@ int main(){
 	if (media >= 9.0){
 		strcpy (aprovacao, "Aprovado(a)");
 		conceito = 'A';
-	} else if(media <= 7.5 && media < 9){
+	} else if((media >= 7.5) && (media < 9.0)){
 		strcpy (aprovacao, "Aprovado(a)");
 		conceito = 'B';
-	}  else if(media >= 6.0 && media < 7.5){
+	} else if((media >= 6.0) && (media < 7.5)){
 		strcpy (aprovacao, "Aprovado(a)");
 		conceito = 'C';
-	} else if(media >= 4.0 && media < 6.0){
+	} else if((media >= 4.0) && (media < 6.0)){
 		strcpy (aprovacao, "Reprovado(a)");
 		conceito = 'D';
 	} else if(media < 4.0){
@@ -43,10 +46,11 @@ int main(){
 	}
 	
 	//Exibindo variáveis.
-	printf("Nome do aluno: %s", nomeAluno);
-	printf("\nNota um: %f", notaUm);
-	printf("\nNota dois: %f", notaDois);
-	printf("\nMédia: %f", media);
+	printf("\n=== Exibindo resultados ===\n");
+	printf("\nNome do aluno: %s", nomeAluno);
+	printf("\nNota um: %.2f", notaUm);
+	printf("\nNota dois: %.2f", notaDois);
+	printf("\nMédia: %.2f", media);
 	printf("\nAprovação: %s", aprovacao);
 	printf("\nConceito: %c", conceito);
 	
