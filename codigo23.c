935d16535b370366c6926115ce1ab771f203d6b7
lmnunes2002 <161032233+lmnunes2002@users.noreply.github.com>
2024-03-25 10:42:21 -0300

    Add files via upload

---
+++ codigo23.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int i, valor, somaPar, somaImpar;
+	
+for(i = 1; i <=5; i++){
+	printf("Digite aqui os %i valores: ", i);
+	scanf("%i", &valor);
+		
+	if (valor % 2 == 0){
+		somaPar += valor;
+	} else if (valor % 2 == 1){
+		somaImpar += valor;
+	} else {
+		printf("Valor inválido");
+	}
+}
+	
+	system("cls");
+	
+	printf("Soma dos valores pares: %i", somaPar);
+	printf("\nSoma dos valores ímpares: %i", somaImpar);
+	
+	return 0;
+		
+}
