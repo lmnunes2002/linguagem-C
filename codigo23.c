023b9a58ffb7c0c4d41f00c76b6de90246ecd568
lmnunes2002 <161032233+lmnunes2002@users.noreply.github.com>
2024-03-25 10:43:41 -0300

    Add files via upload

--- codigo23.c
+++ codigo23.c
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 
 int main(){
-int i, valor, somaPar, somaImpar;
+	int i, valor, somaPar, somaImpar;
 	
-for(i = 1; i <=5; i++){
-	printf("Digite aqui os %i valores: ", i);
-	scanf("%i", &valor);
-		
-	if (valor % 2 == 0){
-		somaPar += valor;
-	} else if (valor % 2 == 1){
-		somaImpar += valor;
-	} else {
-		printf("Valor inválido");
+	for(i = 1; i <=5; i++){
+		printf("Digite aqui os %i valores: ", i);
+		scanf("%i", &valor);
+			
+		if (valor % 2 == 0){
+			somaPar += valor;
+		} else if (valor % 2 == 1){
+			somaImpar += valor;
+		} else {
+			printf("Valor inválido");
+		}
 	}
-}
 	
 	system("cls");
 	
