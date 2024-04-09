7b629a307e9af4769f7ec956512b0353a240e7e5
Lucas Nunes <161032233+lmnunes2002@users.noreply.github.com>
2024-04-09 08:46:51 -0300

    Update codigo39.c

--- codigo39.c
+++ codigo39.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 
 int main(){
+	//Declarando valor no vetor.
 	int numeros[5];
 	numeros[0] = 1;
 	numeros[1] = 2;
 	numeros[2] = 3;
 	numeros[3] = 4;
 	numeros[4] = 5;
-	
+
+	//Exibindo valor do vetor.
 	printf("%i", numeros[0]);
 	printf("\n%i", numeros[1]);
 	printf("\n%i", numeros[2]);
