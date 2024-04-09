4e1aeb8caf4e39419d6c25b4bfc8939490b3fed8
Lucas Nunes <161032233+lmnunes2002@users.noreply.github.com>
2024-04-09 08:09:20 -0300

    Add files via upload

---
+++ codigo40.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int numeros[5];
+	numeros[0] = 1;
+	numeros[1] = 2;
+	numeros[2] = 3;
+	numeros[3] = 4;
+	numeros[4] = 5;
+	
+	printf("%i", numeros[0]);
+	printf("\n%i", numeros[1]);
+	printf("\n%i", numeros[2]);
+	printf("\n%i", numeros[3]);
+	printf("\n%i", numeros[4]);
+}
